f77a0d38e7051ca0a7fdc84560bebc67a2a0aea5
danielpoehle <d.poehle@gmx.de>
2017-09-09 11:13:04 +0200

    implement string name calculator

--- app.js
+++ app.js
@@ -4,7 +4,26 @@
 
 angular.module('myApp', [])
 
-.controller('myController'. function(){
+.controller('myController', function($scope){
+  $scope.name = "";
+  $scope.totalValue = 0;
+  $scope.displayNumeric = function() {
+    var totalNameValue = calculateNumericForString($scope.name); // get the total ascii value of the name
+    $scope.totalValue = totalNameValue;
+  };
+
+  function calculateNumericForString(string) {
+    var totalStringValue = 0;
+    for (var i = 0; i < string.length; i++) {
+      totalStringValue += string.charCodeAt(i);
+    }
+
+    return totalStringValue;
+  }
+
+
+
+
 
 });
 
